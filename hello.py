8a1d3c2f31a10f7398b50e507d6a0f1509bd76fb
Irfan <irfanafif@gmail.com>
2021-12-20 13:20:08 +0800

    211220

--- hello.py
+++ hello.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # create a flask instance
 app = Flask(__name__)
+# create a secret key (CSRF)
+app.config['SECRET_KEY'] = "my super secret key dat nobody should know!"
+
+# create a form class
+
+
+class NamerForm(FlaskForm):
+    name = StringField("What's yer name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # create a route decorator
 
@@ -34,3 +46,18 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# create name page
+
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully!")
+
+    return render_template("name.html", name=name, form=form)
